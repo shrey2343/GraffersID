9c48a8e0c936e0b07b013c6e9d50e565bd2c7ef6
shrey2343 <shreyn82@gmail.com>
2025-09-26 13:44:59 +0530

    Mongodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,10 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is not defined!");
+  }
+  
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
